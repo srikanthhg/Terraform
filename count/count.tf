d6ec493cf618a5edb35883917cea1eb039c122c6
unknown <skanth306@gmail.com>
2024-01-05 19:05:09 +0530

    sd

--- count/count.tf
+++ count/count.tf
@@ -1,10 +1,12 @@
 resource "aws_instance" "web" {
-  count = 11  
+  # count = 11  
+  count = length(var.instance_names)
   ami = var.ami_id
   instance_type = var.instance_names[count.index]
   
   tags = {
     Name = "web - ${count.index+1}"
+   # Component = var.instance_names[count.index]
   }
 }
 
